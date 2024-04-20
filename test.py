8993e93679bd29773abf74915f29ef13b6a267fd
saidc <saidjoc@gmail.com>
2024-04-19 19:01:06 -0500

    update

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ service = connect_to_sheet_api(creds)
 # Obtener la fecha y hora actual
 fecha_actual = datetime.now()
 
-payment_link_id = "ZCXl9i"
+payment_link_id = "test_p81hD8"
 fecha_de_creacion = fecha_actual.strftime("%Y-%m-%dT%H:%M:%S.%fZ") #"2024-02-28T15:59:35.744Z" 
 # Calcular la hora en 2 horas como fecha de expiracion 
 fecha_de_expiracion = (fecha_actual + timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S.%fZ") #"2024-02-28T17:59:35.450Z"
@@ -42,7 +42,7 @@ productos_a_comprar = '[{"id": "elni\u00f1oaquel", "name": "El ni\u00f1o aquel",
 #time.sleep(5)
 #add_row = ['', '', 'test_wodeTj', '2024-04-19T22:10:51.495Z', '', '2024-04-20T01:10:51.195Z', 'ESPERANDO', 'VENTAS WEB', '', '', '', '', '', '', '', '[{"id": "elni\\u00f1oaquel", "name": "El ni\\u00f1o aquel", "image": "https://live.staticflickr.com/65535/53550559789_776de25b1c_o.png", "price": 40000, "amount": 1, "subtotal": 40000, "price_dolar": -1, "dolar_subtotal": -1}]', '', '', '', '', '', '', '', '', '', '', '', '']
 #updaterowbaywebhookrespond = ['139086-1713564676-32081', '2024-04-19T22:11:16.648Z', '2024-04-19T22:11:17.627Z', '5000000', 'saidjoc@gmail.com', 'COP', 'PSE', 'APPROVED', 'carrera 11#20-57', 'Casa', 'CO', 'Cesar', 'Valledupar', 'SAID JOSÉ CORTÉS CALDERÓN', '+573106016968', '200001', 'https://ipuc-libros.my.canva.site/thanks-for-buy-elninoaquel', 'test_wodeTj']
-webhook_res = ['139086-1713564676-32081', '2024-04-19T22:11:16.648Z', '2024-04-19T22:11:17.627Z', '5000000', 'saidjoc@gmail.com', 'COP', 'PSE', 'APPROVED', 'carrera 11#20-57', 'Casa', 'CO', 'Cesar', 'Valledupar', 'SAID JOSÉ CORTÉS CALDERÓN', '+573106016968', '200001', 'https://ipuc-libros.my.canva.site/thanks-for-buy-elninoaquel', 'ZCXl9i']
+webhook_res = ['139086-1713564676-32081', '2024-04-19T22:11:16.648Z', '2024-04-19T22:11:17.627Z', '5000000', 'saidjoc@gmail.com', 'COP', 'PSE', 'APPROVED', 'carrera 11#20-57', 'Casa', 'CO', 'Cesar', 'Valledupar', 'SAID JOSÉ CORTÉS CALDERÓN', '+573106016968', '200001', 'https://ipuc-libros.my.canva.site/thanks-for-buy-elninoaquel', 'test_p81hD8']
 
 rows = get_rows(service, SPREADSHEET_ID, SHEET_NAME)
 
